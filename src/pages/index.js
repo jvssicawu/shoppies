3eba53dde63f9505a2516a19ca09982a9feaaff7
jvssicawu <jessicawu.98@hotmail.ca>
2021-01-15 23:43:02 -0800

    Update site metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,20 @@ import { theme } from '../Constants';
 import Banner from '../components/Banner';
 import Container from '../containers/Container';
 import Intro from '../components/Intro';
+import Metadata from '../contexts/Metadata';
 import Nominations from '../components/Nominations';
 import Posts from '../components/Posts';
 import StateProvider from '../contexts/Store';
 
 const Home = () => (
   <StateProvider>
+    <Metadata />
     <ThemeProvider theme={theme}>
       <Intro />
+      <Banner />
       <Container>
         <Posts />
         <Nominations />
-        <Banner />
       </Container>
     </ThemeProvider>
   </StateProvider>
