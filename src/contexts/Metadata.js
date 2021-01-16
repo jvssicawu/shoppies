ea3fffac8117d73fc1b5b64c245b6bddecd14dda
jvssicawu <jessicawu.98@hotmail.ca>
2021-01-15 23:43:02 -0800

    Update site metadata

---
+++ src/contexts/Metadata.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Metadata = () => (
+  <Helmet>
+    <meta charSet="utf-8" />
+    <title>Shoppies</title>
+    <link rel="canonical" href="https://jvssicawu-shoppies.netlify.app" />
+  </Helmet>
+);
+
+export default Metadata;
